dd74117333b0f0dbc788eaaea73bcb944227c9e2
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-02-16 19:11:25 +0000

    Test: Add test for getting module selection configurations for year

--- tests/api/test_configurations.py
+++ tests/api/test_configurations.py
@@ -1,7 +1,9 @@
 from tests.conftest import HPOTTER_CREDENTIALS
 
 
-def test_student_can_get_subscribed_modules(client, configuration_factory):
+def test_can_get_module_selection_configuration_by_degree_year(
+    client, configuration_factory
+):
     configuration = configuration_factory(with_periods=1)
     res = client.get(
         f"/{configuration.year}/configurations/{configuration.degree_year}",
@@ -12,3 +14,13 @@ def test_student_can_get_subscribed_modules(client, configuration_factory):
     assert response["degree_year"] == configuration.degree_year
     assert response["status"] == configuration.status
     assert len(response["periods"]) == 1
+
+
+def test_can_get_module_selection_configurations(client, configuration_factory):
+    configuration_factory.create_batch(size=2, year="2324")
+    res = client.get(
+        "/2324/configurations",
+        auth=HPOTTER_CREDENTIALS,
+    )
+    assert res.status_code == 200
+    assert len(res.json()) == 2
