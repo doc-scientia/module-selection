6f74e7503b6398345eec26efb254d3ca04094156
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-03-05 17:19:45 +0000

    Feat: Implement endpoint to query own external module choices

--- app/api/personal.py
+++ app/api/personal.py
@@ -7,16 +7,34 @@ from app.schemas.module_choices import ExternalModuleChoice, ExternalModuleChoic
 personal_router = APIRouter(prefix="/me/{year}")
 
 
+@personal_router.get(
+    "/external-modules/choices",
+    tags=["personal module choices"],
+)
+async def get_personal_external_module_choices(
+    year: str,
+    session: Session = Depends(get_session),
+    response_model=list[ExternalModuleChoice],
+    current_user: str = Depends(get_current_user),
+):
+    username = current_user
+    query = select(ExternalModuleChoice).where(
+        ExternalModuleChoice.year == year,
+        ExternalModuleChoice.username == username,
+    )
+    return session.exec(query).all()
+
+
 @personal_router.post(
     "/external-modules/choices",
     tags=["personal module choices"],
 )
 async def apply_for_external_module(
-        year: str,
-        subscription: ExternalModuleChoiceWrite,
-        session: Session = Depends(get_session),
-        response_model=ExternalModuleChoice,
-        current_user: str = Depends(get_current_user),
+    year: str,
+    subscription: ExternalModuleChoiceWrite,
+    session: Session = Depends(get_session),
+    response_model=ExternalModuleChoice,
+    current_user: str = Depends(get_current_user),
 ):
     username = current_user
     module_code = subscription.module_code
--- tests/api/test_personal.py
+++ tests/api/test_personal.py
@@ -1,6 +1,18 @@
 from tests.conftest import HPOTTER_CREDENTIALS
 
 
+def test_student_can_get_own_external_module_choices(
+    client, external_module_choice_factory
+):
+    external_module_choice_factory.create_batch(size=3, username="hpotter", year="2324")
+    res = client.get(
+        "me/2324/external-modules/choices",
+        auth=HPOTTER_CREDENTIALS,
+    )
+    assert res.status_code == 200
+    assert len(res.json()) == 3
+
+
 def test_student_can_submit_valid_external_module_registration(client):
     module_code = "70007"
     res = client.post(
@@ -14,7 +26,7 @@ def test_student_can_submit_valid_external_module_registration(client):
 
 
 def test_student_cant_submit_module_reg_if_external_module_choice_exists(
-        client, external_module_choice_factory
+    client, external_module_choice_factory
 ):
     module_code = "70007"
     external_module_choice_factory(
