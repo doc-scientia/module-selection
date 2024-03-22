dda95f30a18a3d05f4a43da20dccffaabdc16bfc
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-03-22 17:47:42 +0100

    Feat: Create offering group constraint table

---
+++ app/schemas/offering_group.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Enum
+from sqlmodel import Field, SQLModel
+
+from app.schemas.internal_modules import OfferingGroup
+
+
+class OfferingGroupConstraint(SQLModel, table=True):
+    __tablename__ = "offering_group_constraint"
+    id: int = Field(primary_key=True)
+    year: str = Field(nullable=False)
+    offering_group: OfferingGroup = Field(
+        sa_column=Column(
+            Enum(OfferingGroup, name="offering_group"),
+            nullable=False,
+        )
+    )
+    min: int = Field(nullable=False)
+    max: int = Field(nullable=False)
