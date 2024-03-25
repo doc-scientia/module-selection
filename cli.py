a2f52fd62bc35b4cab4b170912c2dff65507e4bd
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-03-25 18:28:14 +0100

    Feat: Add CLI command to populate dev data

--- cli.py
+++ cli.py
@@ -1,28 +1,60 @@
-import typer
-from sqlalchemy_utils import create_database, database_exists, drop_database
-from sqlmodel import SQLModel
+from contextlib import contextmanager
+from datetime import datetime, timedelta
 
-from app.database.connection import engine
+import typer
+from typer import Argument
+
+from app.dependencies.main import get_session
+from app.factories import (
+    ConfigurationFactory,
+    ExternalModuleOnOfferFactory,
+    InternalModuleOnOfferFactory,
+    all_factories,
+)
+from app.schemas.configurations import ModuleSelectionStatus
+from app.schemas.internal_modules import OfferingGroup
 
 cli = typer.Typer()
 
 
+@contextmanager
+def dynamic_session():
+    session = next(get_session())
+    for f in all_factories:
+        f._meta.sqlalchemy_session = session
+    yield
+
+
 @cli.command()
-def create_tables():
-    SQLModel.metadata.create_all(engine)
-
-
-# python cli.py delete-database
-@cli.command()
-def delete_database():
-    if database_exists(engine.url):
-        drop_database(engine.url)
-
-
-# python cli.py create-db
-@cli.command()
-def create_db():
-    print("db exists") if database_exists(engine.url) else create_database(engine.url)
+def populate_db(
+    year: str = Argument(help="Academic year in short form e.g. 2324 for 2023-2024"),
+):
+    """
+    Populates the database with dummy data.
+    """
+    with dynamic_session():
+        ConfigurationFactory(
+            year=year,
+            with_periods=[dict(end=datetime.utcnow() + timedelta(weeks=2))],
+            status=ModuleSelectionStatus.USE_PERIODS,
+        )
+        ExternalModuleOnOfferFactory.create_batch(size=3, year=year)
+        InternalModuleOnOfferFactory.create_batch(
+            size=5,
+            year=year,
+            with_regulations=[
+                dict(cohort="c3", offering_group=OfferingGroup.OPTIONAL),
+                dict(cohort="v5", offering_group=OfferingGroup.SELECTIVE),
+            ],
+        )
+        InternalModuleOnOfferFactory.create_batch(
+            size=3,
+            year=year,
+            with_regulations=[
+                dict(cohort="c3", offering_group=OfferingGroup.SELECTIVE),
+            ],
+        )
+    print("Database populated successfully.")
 
 
 if __name__ == "__main__":
