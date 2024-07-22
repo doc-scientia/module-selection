75c6003c6e68391151a477f731e41c3451e7a467
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-07-22 14:56:02 +0100

    fix: replace postgres with postgresql

--- app/database/connection.py
+++ app/database/connection.py
@@ -3,6 +3,6 @@ import os
 from sqlmodel import create_engine
 
 DEV_URL = "postgresql://user:pass@localhost"
-FULL_DB_URL: str = os.environ.get("DB_URL", os.environ.get("POSTGRES_URL", f"{DEV_URL}/module_subscriptions"))
+FULL_DB_URL: str = os.environ.get("DB_URL", os.environ.get("POSTGRES_URL", f"{DEV_URL}/module_subscriptions")).replace("postgres://", "postgresql://")
 
 engine = create_engine(FULL_DB_URL)
