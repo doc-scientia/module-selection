90597d8bafde561dbbc66f8ceba66b3a2af19ce5
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2024-07-23 12:23:51 +0100

    feat: construct azure DB URL from individual vars

--- app/database/connection.py
+++ app/database/connection.py
@@ -5,4 +5,22 @@ from sqlmodel import create_engine
 DEV_URL = "postgresql://user:pass@localhost"
 FULL_DB_URL: str = os.environ.get("DB_URL", os.environ.get("POSTGRES_URL", f"{DEV_URL}/module_subscriptions")).replace("postgres://", "postgresql://")
 
-engine = create_engine(FULL_DB_URL)
+def get_db_string() -> str:
+
+	if os.environ.get("AZURE_POSTGRESQL_HOST") is None:
+		return FULL_DB_URL
+
+	hostname = os.environ.get("AZURE_POSTGRESQL_HOST")
+	port = os.environ.get("AZURE_POSTGRESQL_PORT")
+	database = os.environ.get("AZURE_POSTGRESQL_DATABASE")
+	username = os.environ.get("AZURE_POSTGRESQL_USERNAME")
+	password = os.environ.get("AZURE_POSTGRESQL_PASSWORD")
+
+	return f"postgresql://{username}:{password}@{hostname}:{port}/{database}"
+
+engine = create_engine(get_db_string())
+
+
+
+
+
